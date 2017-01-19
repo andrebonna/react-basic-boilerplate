d7189f89de99786d5c866070fb4a84ee8f3855fb
andre.bonna@gmail.com <andre.bonna@gmail.com>
2017-01-19 22:47:36 +0000

    Improving webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 const webpackConfig = {
     entry: './View/src/index.js',
@@ -8,7 +11,7 @@ const webpackConfig = {
             loader: 'babel-loader',
             test: /\.js$/
         }, {
-            loaders: ['style-loader', 'css-loader', 'sass-loader'],
+            loader: ExtractTextPlugin.extract(['css-loader?sourceMap!sass-loader?sourceMap']),
             test: /\.scss$/
         }]
     },
@@ -17,14 +20,27 @@ const webpackConfig = {
         path: './View/public'
     },
     resolve: {extensions: ['', '.js', '.json']},
-    sassLoader: {outputStyle: 'extended'}
+    sassLoader: {outputStyle: 'extended'},
+    plugins: [new CleanWebpackPlugin(['View/public'], {
+        verbose: true
+    }), new HtmlWebpackPlugin({
+        filename: 'index.html',
+        template: 'View/src/index-template.html'
+    })]
 };
 
 if (process.env.NODE_ENV === 'production') {
     webpackConfig.devtool = 'source-map';
-    webpackConfig.plugins = [new webpack.optimize.OccurenceOrderPlugin(), new webpack.DefinePlugin({
+
+    const time = new Date().getTime();
+    const stylesProd = new ExtractTextPlugin(`${time}-bundle.css`);
+    webpackConfig.output.filename = `${time}-bundle.js`;
+    webpackConfig.plugins.push(stylesProd, new webpack.optimize.OccurenceOrderPlugin(), new webpack.DefinePlugin({
         'process.env': {NODE_ENV: JSON.stringify('production')}
-    }), new webpack.optimize.UglifyJsPlugin({compress: {warnings: false}})];
+    }), new webpack.optimize.UglifyJsPlugin({compress: {warnings: false}}));
+} else {
+    const styles = new ExtractTextPlugin('app.css');
+    webpackConfig.plugins.push(styles);
 }
 
 module.exports = webpackConfig;
